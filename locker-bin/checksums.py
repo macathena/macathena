731f6080f2d0bf7d3b89432323c6cc1a94010cff
Evan Broder <broder@mit.edu>
2008-01-08 07:57:44 +0000

    checksums.py should output rmd160 instead of ripemd160 (yes, I know, I'm making pointless commits, but I want to get this hook working)

--- locker-bin/checksums.py
+++ locker-bin/checksums.py
@@ -8,4 +8,4 @@ for arg in sys.argv[1:]:
 	f = open(arg).read()
 	print "checksums\t\tmd5 %s \\" % hashlib.md5(f).hexdigest()
 	print "\t\t\t\tsha1 %s \\" % hashlib.sha1(f).hexdigest()
-	print "\t\t\t\tripemd160 %s" % hashlib.new('rmd160', f).hexdigest()
+	print "\t\t\t\trmd160 %s" % hashlib.new('rmd160', f).hexdigest()
