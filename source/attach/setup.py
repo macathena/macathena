3f26daa5ce959814adcc44cc6ce17a1b9d2106ef
Evan Broder <broder@mit.edu>
2008-01-25 07:54:16 +0000

    Add setup.py script for attach

---
+++ source/attach/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='macathena-attach',
+      version='1.0',
+      author='SIPB MacAthena Project',
+      author_email='sipb-macathena@mit.edu',
+      scripts=['attach'])
