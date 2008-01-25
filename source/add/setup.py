4cde4b3641dc82abeb4a416977923ba2de7553a7
Evan Broder <broder@mit.edu>
2008-01-25 07:34:36 +0000

    I'm an idiot and didn't put the right paths in the setup.py file for add

--- source/add/setup.py
+++ source/add/setup.py
@@ -4,4 +4,4 @@ setup(name='macathena-add',
       author='SIPB MacAthena Project',
       author_email='sipb-macathena@mit.edu',
       scripts=['attach-add.py'],
-      data_files=[('@FINKPREFIX@/etc/profile.d', ('macathena-attach.sh',))])
+      data_files=[('@FINKPREFIX@/etc/profile.d', ('macathena-add.sh','macathena-add.csh'))])
