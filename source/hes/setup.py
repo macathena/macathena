a167e58f135f840f10278b6784d576afaa9e7cc1
Evan Broder <broder@mit.edu>
2008-02-23 00:30:28 +0000

    hes...Now With More Python!

---
+++ source/hes/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='macathena-hes',
+      version='1.0',
+      author='SIPB MacAthena Project',
+      author_email='sipb-macathena@mit.edu',
+      scripts=['hes'])
