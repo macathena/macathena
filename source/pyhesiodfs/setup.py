27393ac3feb07cf8caeb027bb2acbc15cda0a72f
Evan Broder <broder@mit.edu>
2008-07-31 02:42:02 +0000

    In pyHesiodFS, only install LaunchDaemon plist if on a Mac

--- source/pyhesiodfs/setup.py
+++ source/pyhesiodfs/setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
+import sys
+
+extra_options = {}
+
+if sys.platform == 'darwin':
+    extra_options['data_files'] = [('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))]
+
 setup(name='pyHesiodFS',
       version='1.0',
       author='Quentin Smith',
       author_email='pyhesiodfs@mit.edu',
       py_modules=['hesiod'],
       scripts=['pyHesiodFS.py'],
-      data_files=[('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))])
+      **extra_options)
