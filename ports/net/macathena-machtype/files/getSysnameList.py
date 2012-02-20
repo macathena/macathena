1866568072bb82834f2f316cd2b940cf85013810
Quentin Smith <quentin@mit.edu>
2012-02-20 19:05:37 +0000

    Update and fix the macathena-machtype port

--- ports/net/macathena-machtype/files/getSysnameList.py
+++ ports/net/macathena-machtype/files/getSysnameList.py
@@ -5,15 +5,17 @@ from commands import getoutput
 arch = getoutput('arch')
 darwin_ver = int(getoutput('uname -r').split('.')[0]) * 10
 
-ver_list = range(darwin_ver, 40, -10)
+ver_list = range(darwin_ver, 70, -10)
 x86_list = ["x86_darwin_%d" % ver for ver in ver_list]
 ppc_list = ["ppc_darwin_%d" % ver for ver in ver_list]
 
-if arch == 'i386':
+if arch == 'i386' and darwin_ver >= 110:
+	versions = x86_list
+elif arch == 'i386':
 	versions = [item for items in zip(x86_list, ppc_list) for item in items]
 else:
 	versions = ppc_list
 
-versions += ['share', 'common', 'any']
+versions += ['share', 'common', 'any', 'all']
 
 print 'ATHENA_SYS=%s ATHENA_SYS_COMPAT="%s"' % (versions[0], ':'.join(versions[1:]))
--- ports/net/macathena-machtype/files/patch-machtype_linux.sh
+++ ports/net/macathena-machtype/files/patch-machtype_linux.sh
@@ -1,12 +1,12 @@
---- machtype_linux.sh.orig	2007-12-24 21:51:18.000000000 -0600
-+++ machtype_linux.sh	2007-12-25 00:20:57.000000000 -0600
+--- machtype_linux.sh.orig	2011-10-28 17:31:43.000000000 -0400
++++ machtype_linux.sh	2012-02-20 12:33:57.000000000 -0500
 @@ -1,4 +1,4 @@
 -#!/bin/sh
 +#!/bin/bash
- # $Id: machtype_linux.sh,v 1.9 2003/08/12 21:47:50 jweiss Exp $
+ # $Id: machtype_linux.sh,v 1.9 2003-08-12 21:47:50 jweiss Exp $
  
  # We need to support the following options:
-@@ -86,7 +86,7 @@
+@@ -91,7 +91,7 @@
  fi
  
  if [ $syspacks ]; then
@@ -15,20 +15,22 @@
  	printed=1
  fi
  
-@@ -133,32 +133,31 @@
+@@ -145,27 +145,26 @@
  fi
  
  if [ $display ] ; then
--        /sbin/lspci | awk -F: '/VGA/ {print $3}' | sed -n -e 's/^ //' -e p
+-	lspci | awk -F: '/VGA/ {print $3}' | sed -n -e 's/^ //' -e p
 +	system_profiler SPDisplaysDataType | awk 'NR==3 { print }' | sed "s/^ *\(.*\):$/\1/"
  	printed=1
  fi
  
  if [ $rdsk ]; then
--	awk '/^SCSI device/ { print; }
--	     /^hd[a-z]:/ { print; }
--	     /^Floppy/ { for (i=3; i <= NF; i += 3) print $i ": " $(i+2); }' \
--	     /var/log/dmesg
+-	for d in /sys/block/[fhs]d*; do
+-	    echo $(basename "$d"): \
+-		$(xargs -I @ expr @ '*' 8 / 15625 < "$d/size")MB \
+-		$(cat "$d/device/model" ||
+-		  cat "/proc/ide/$(basename "$d")/model")
+-	done 2>/dev/null
 +	disks=`ls /dev/disk* | grep 'disk[0-9]*$'`
 +	for disk in $disks
 +	do
@@ -41,17 +43,17 @@
 +	physmem=$(($(sysctl -n hw.physmem)/1024))
 +	usermem=$(($(sysctl -n hw.usermem)/1024))
  	if [ $verbose ]; then
--		awk 'BEGIN { FS="[^0-9]+" }
--		     /^Memory:/ { printf "user=%d, phys=%d (%d M)\n",
--				         $2*1.024, $3*1.024, $3/1000; }' \
--		    /var/log/dmesg
+-		awk '/^MemTotal:/ { printf "user=%d, phys=%d (%d M)\n",
+-					   $2, $2, $2/1024 }' \
+-		    /proc/meminfo
 +		printf "user=%d, phys=%d (%d M)\n" $usermem $physmem $((physmem/1024))
  	else
--		awk 'BEGIN { FS="[^0-9]+" }
--		     /^Memory:/ { printf "%d\n", $3*1.024; }' /var/log/dmesg
+-		awk '/^MemTotal:/ { printf "%d\n", $2 }' /proc/meminfo
 +		echo $physmem
  	fi
  	printed=1
+ fi
+@@ -182,6 +181,6 @@
  fi
  
  if [ $printed -eq '0' ] ; then
