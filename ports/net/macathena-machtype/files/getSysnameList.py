af9e577b005bb07d6a8c1a8246f365fd6fd60905
Quentin Smith <quentin@mit.edu>
2020-07-20 22:41:20 -0400

    Now with more Python 3

--- ports/net/macathena-machtype/files/getSysnameList.py
+++ ports/net/macathena-machtype/files/getSysnameList.py
@@ -4,16 +4,16 @@ from __future__ import print_function
 
 import subprocess
 
-arch = subprocess.check_output(['arch'])
+arch = subprocess.check_output(['arch']).strip()
 darwin_ver = int(subprocess.check_output(['uname', '-r']).split(b'.')[0]) * 10
 
 ver_list = range(darwin_ver, 70, -10)
 x86_list = ["x86_darwin_%d" % ver for ver in ver_list]
 ppc_list = ["ppc_darwin_%d" % ver for ver in ver_list]
 
-if arch == 'i386' and darwin_ver >= 110:
+if arch == b'i386' and darwin_ver >= 110:
 	versions = x86_list
-elif arch == 'i386':
+elif arch == b'i386':
 	versions = [item for items in zip(x86_list, ppc_list) for item in items]
 else:
 	versions = ppc_list
