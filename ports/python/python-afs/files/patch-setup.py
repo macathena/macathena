105e14f5297e627fb7f25b1b7676489328b93d2d
Miriam Rittenberg <mrittenb@mit.edu>
2020-07-25 23:08:09 -0400

    Turns out OpenAFS does have -lhcrypto, it's just called -lafshcrypto

--- ports/python/python-afs/files/patch-setup.py
+++ ports/python/python-afs/files/patch-setup.py
@@ -2,7 +2,7 @@ diff --git a/setup.py b/setup.py
 index 378c7dc..879523a 100755
 --- a/setup.py
 +++ b/setup.py
-@@ -17,20 +17,23 @@ import os
+@@ -17,20 +17,22 @@ import os
  for root in ['/Library/OpenAFS/Tools',
               '/usr/local',
               '/usr/afsws',
@@ -17,7 +17,6 @@ index 378c7dc..879523a 100755
 +                '%s/include/afs' % root]
 +os.environ['LDFLAGS'] = '-framework Kerberos'
  library_dirs = ['%s/lib' % root,
-+                '%s/libexec/heimdal/lib' % root,
 -                '%s/lib/afs' % root]
 +               '%s/lib/afs' % root]
  if os.path.exists('%s/lib/libafsauthent_pic.a' % root):
@@ -25,7 +24,7 @@ index 378c7dc..879523a 100755
  else:
      suffix = ''
 -libraries = ['afsauthent%s' % suffix, 'afsrpc%s' % suffix, 'resolv']
-+libraries = ['afsauthent%s' % suffix, 'afsrpc%s' % suffix, 'resolv', 'hcrypto']
++libraries = ['afsauthent%s' % suffix, 'afsrpc%s' % suffix, 'resolv', 'afshcrypto']
  define_macros = [('AFS_PTHREAD_ENV', None)]
 
  def PyAFSExtension(module, *args, **kwargs):
